c4537e9d373ef57980723e67d0a98b2b5f15125d
gabri <gabri@SILVERSTORM>
2025-11-09 20:18:50 -0500

    Add project files.

---
+++ GameBlog/DataAccess/GameBlogDBContext.cs
@@ -0,0 +1,9 @@
+﻿using GameBlog.Models.DBModels;
+using Microsoft.EntityFrameworkCore;
+
+public class GameBlogDBContext : DbContext
+{
+    public GameBlogDBContext(DbContextOptions<GameBlogDBContext> options) : base(options) { }
+
+    public DbSet<BlogPost> BlogPosts { get; set; }
+}
---
+++ GameBlog/Models/DBModels/BlogPost.cs
@@ -0,0 +1,9 @@
+﻿namespace GameBlog.Models.DBModels
+{
+    public class BlogPost
+    {
+        public int Id { get; set; }
+        public string title { get; set; }
+        public string post { get; set; }
+    }
+}
